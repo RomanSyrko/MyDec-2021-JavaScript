3a74b61b15874f5f089543849d421144617a10c9
lvsirko <19328654sS>
2022-01-31 23:26:44 +0200

    Add new one

---
+++ Lesson1/main.js
@@ -0,0 +1,163 @@
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// var text = 'hello';
+// var text1 = 'owu';
+// var text2 = 'com';
+// var text3 = 'ua';
+// var number = 1;
+// var number1 = 10;
+// var number2 = -999;
+// var number3 = 123;
+// var number4 = 3.14;
+// var number5 = 2.7;
+// var number6 = 16;
+// var b = true;
+// var b1 = false;
+
+// -- 1.0 Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(text)
+// console.log(text1)
+// console.log(text2)
+// console.log(text3)
+// console.log(number)
+// console.log(number1)
+// console.log(number2)
+// console.log(number3)
+// console.log(number4)
+// console.log(number5)
+// console.log(number6)
+// console.log(b)
+// console.log(b1)
+
+// -- 1.1 Вивести кожну змінну за допомогою: console.log , alert, document.write
+// alert(text)
+// alert(text1)
+// alert(text2)
+// alert(text3)
+// alert(number)
+// alert(number1)
+// alert(number2)
+// alert(number3)
+// alert(number4)
+// alert(number5)
+// alert(number6)
+// alert(b)
+// alert(b1)
+
+// -- 1.2 Вивести кожну змінну за допомогою: console.log , alert, document.write
+// document.write(text)
+// document.write(text1)
+// document.write(text2)
+// document.write(text3)
+// document.write(number)
+// document.write(number1)
+// document.write(number2)
+// document.write(number3)
+// document.write(number4)
+// document.write(number5)
+// document.write(number6)
+// document.write(b)
+// document.write(b1)
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+// var text = 'goodbye';
+// var text1 = 'my';
+// var text2 = 'good';
+// var text3 = 'sense';
+// var number = 22;
+// var number1 = 444;
+// var number2 = -5.4;
+// var number3 = 432;
+// var number4 = -3.14;
+// var number5 = 2.789;
+// var number6 = 100500;
+// var b = true;
+// var b1 = false;
+
+// --2.0 Вивести кожну змінну за допомогою: console.log , alert, document.write
+// console.log(text)
+// console.log(text1)
+// console.log(text2)
+// console.log(text3)
+// console.log(number)
+// console.log(number1)
+// console.log(number2)
+// console.log(number3)
+// console.log(number4)
+// console.log(number5)
+// console.log(number6)
+// console.log(b)
+// console.log(b1)
+
+// -- 2.1 Вивести кожну змінну за допомогою: console.log , alert, document.write
+// alert(text)
+// alert(text1)
+// alert(text2)
+// alert(text3)
+// alert(number)
+// alert(number1)
+// alert(number2)
+// alert(number3)
+// alert(number4)
+// alert(number5)
+// alert(number6)
+// alert(b)
+// alert(b1)
+
+// -- 2.2 Вивести кожну змінну за допомогою: console.log , alert, document.write
+// document.write(text)
+// document.write(text1)
+// document.write(text2)
+// document.write(text3)
+// document.write(number)
+// document.write(number1)
+// document.write(number2)
+// document.write(number3)
+// document.write(number4)
+// document.write(number5)
+// document.write(number6)
+// document.write(b)
+// document.write(b1)
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let firstName = 'Roman';
+// let middleName = 'Mykolayovych';
+// let lastName = 'Syrko';
+// let person = [firstName, middleName, lastName]
+// console.log(person)
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// let firstName = prompt(`Please input your first name`);
+// let lastName = prompt(`Please input your last name`);
+// let myAge = prompt(`Please input your age`);
+// console.log(`Greetings ${firstName} ${lastName}. You are ${myAge} years old`);  // <=== or alert();
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof (a))
+// console.log(typeof (b))
+// console.log(typeof (c))
+
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат. В однакових виразах не використовувати однакові оператори!!!
+// console.log(5 < 6)
+// console.log(5 > 6)
+// console.log(5 >= 6)
+// console.log(5 == 6)
+// console.log(10 === 10)
+// console.log(10 == 10)
+// console.log(10 > 10)
+// console.log(10 < 10)
+// console.log(10 !== 10)
+// console.log(123 === '123')
+// console.log(123 == '123')
+
+// - Подумайте Які значення виведе у вікно браузера наступний фрагмент коду? і чому?
+let str = "20";
+let a = 5;
+document.write(str + a + "<br/>");    // =205  -- string + integer = string
+document.write(str - a + "<br/>");    // =15  --  string - integer = integer
+document.write(str * "2" + "<br/>");  // =40  -- string * integer = integer
+document.write(str / 2 + "<br/>");    // =10  -- string / integer = integer
+
+// !!! Відповідь на останнє завдання я давав судячи з результату виведеного на екран)
